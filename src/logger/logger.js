561a8dd69785bb9ee9e0559e3e68f459896825bc
Ranjan906-thorium <ranjanmaji906@gmail.com>
2022-02-22 09:52:18 +0530

    logger.js

---
+++ src/logger/logger.js
@@ -0,0 +1,11 @@
+const { message } = require("statuses")
+
+
+
+function printMyMessage(message){
+    console.log(message)
+
+}
+
+
+module.exports.printMessage = printMyMessage
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,12 @@
+let obj = require('../logger/logger');
 const express = require('express');
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+    res.send('Welcome to my application. I am Ranjan Maji and a part of FunctionUp Thorium cohort')
 });
+
+
+
 
 module.exports = router;
