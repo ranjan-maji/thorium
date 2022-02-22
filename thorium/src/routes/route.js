5eb07cb6e1df59ee26eef481e63fbd678d5e91e7
Ranjan906-thorium <ranjanmaji906@gmail.com>
2022-02-23 00:17:38 +0530

    try

---
+++ thorium/src/routes/route.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/students/:name', function(req, res) {
+    let studentName = req.params.name
+    console.log(studentName)
+    res.send(studentName)
+})
+
+
+
+
+module.exports = router;
